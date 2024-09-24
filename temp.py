ae3b660364b8bf4256c37f77c45e6c7df73a6782
GudduBhai81 <306botala@gmail.com>
2024-09-24 23:15:15 +0500

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,7 @@
+# Program to check if the temperature is suitable for wearing light clothes
+temperature = float(input("Enter the temperature in Celsius: "))
+
+if temperature > 20:
+    print("The temperature is suitable for wearing light clothes.")
+else:
+    print("The temperature is not suitable for wearing light clothes. Consider wearing warmer clothes.")
